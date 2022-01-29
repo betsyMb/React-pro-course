ef3f6eb4e491cb2d18720c932284ce4811612896
Betsy Moreno <morenoelba2002@gmail.com>
2022-01-29 14:08:20 -0400

    feature: basic lazyload

--- src/01-lazyload/pages/LazyPage1.tsx
+++ src/01-lazyload/pages/LazyPage1.tsx
@@ -1,3 +1,5 @@
 export const LazyPage1 = () => {
   return <h2>lazy page 1</h2>;
 };
+
+export default LazyPage1;
--- src/01-lazyload/pages/LazyPage3.tsx
+++ src/01-lazyload/pages/LazyPage3.tsx
@@ -1,3 +1,5 @@
 export const LazyPage3 = () => {
   return <h2>lazy page 3</h2>;
 };
+
+export default LazyPage3;
